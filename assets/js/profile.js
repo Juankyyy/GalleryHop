d125785b6312d6d0f6a83fbbaccdba000e562258
Juankyyy <juancamiloherrera960@gmail.com>
2024-02-02 21:19:59 -0500

    Profile: Menu Funcionalidad

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,9 +1,23 @@
 const menuBars = document.querySelector(".menu-bars");
-const menu = document.querySelector(".menu-content")
+const menu = document.querySelector(".menu-content");
 
 const bar1 = document.querySelector(".menu-bars-1");
 const bar2 = document.querySelector(".menu-bars-2");
 const bar3 = document.querySelector(".menu-bars-3");
+
+const buttons = document.querySelectorAll(".btn");
+const buttonsTitle = document.querySelector(".creations-title");
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        buttons.forEach(buttonR => {
+            buttonR.classList.remove("btn-selected");
+        })
+
+        button.classList.add("btn-selected");
+        const title = button.children[0].innerHTML;
+        buttonsTitle.children[0].innerHTML = title
+    })
+})
 
 menuBars.addEventListener("click", activeBars);
 
