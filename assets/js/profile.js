e4112d6fa53a627763f63cb5e2a023eebe5ce59f
Juanky <juancamiloherrera960@gmail.com>
2024-02-06 11:31:01 -0500

    Navbar y Footer Individuales en todas las pages

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,10 +1,3 @@
-const menuBars = document.querySelector(".menu-bars");
-const menu = document.querySelector(".menu-content");
-
-const bar1 = document.querySelector(".menu-bars-1");
-const bar2 = document.querySelector(".menu-bars-2");
-const bar3 = document.querySelector(".menu-bars-3");
-
 const buttons = document.querySelectorAll(".btn");
 const buttonsTitle = document.querySelector(".creations-title");
 buttons.forEach(button => {
@@ -18,15 +11,3 @@ buttons.forEach(button => {
         buttonsTitle.children[0].innerHTML = title
     })
 })
-
-menuBars.addEventListener("click", activeBars);
-
-function activeBars() {
-    bar1.classList.toggle("activemenu-bars-1")
-    bar2.classList.toggle("activemenu-bars-2")
-    bar3.classList.toggle("activemenu-bars-3")
-
-    menu.classList.toggle("menu-content-hide");
-    menu.classList.toggle("menu-shadow");
-    menuBars.classList.toggle("menu-fixed");
-}
