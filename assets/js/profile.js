bb52cacc3a9610cdc44918c4739b2e646b3fd198
wilmar <wilmarandrespuertajaramillo@gmail.com>
2024-02-07 13:40:16 -0500

    Profile: Consumo de del objeto de user

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -11,3 +11,27 @@ buttons.forEach(button => {
         buttonsTitle.children[0].innerHTML = title
     })
 })
+
+// Profile user
+
+if (localStorage.id) {
+
+    fetch(`http://localhost:3000/users/${localStorage.id}`)
+        .then(response => {
+            return response.json();
+        }).then(data => {
+            const nameUserProfile = document.getElementById("nameUserProfile").textContent = `${data.username}`;
+            const imgUserProfile = document.getElementById("imgUserProfile");
+            const creationsImg = document.querySelector(".creations-img");
+
+            data.posts.forEach(post => {
+                const postImg = document.createElement("img");
+                postImg.setAttribute("src", `${post.image}`);
+                postImg.setAttribute("alt", `Gallery ${post.name}`);
+
+                creationsImg.appendChild(postImg);
+            })
+            imgUserProfile.setAttribute("src", `${data.imguser}`);
+
+        })
+}
